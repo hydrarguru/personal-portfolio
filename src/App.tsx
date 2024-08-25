23c0058a6f9c663d916d171f0069965705de326d
hydrarguru <henrik@engqvist.org>
2024-08-25 23:12:25 +0200

    feat: navigation menu.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import IconButton from "./components/IconButton";
 import LinkButton from "./components/LinkButton";
-
+import NavbarMenu from "./components/NavigationMenu";
 
 function App() {
   return (
@@ -8,45 +8,79 @@ function App() {
       <div className="flex h-full w-full flex-wrap justify-between rounded-2xl sm:border lg:divide-x">
         <div className="w-full p-2 md:p-8 lg:h-full lg:w-2/5 lg:overflow-y-scroll">
           <section className="flex w-full flex-col lg:min-h-[calc(100vh-7rem)]">
-            <div className="flex justify-between items-center mt-6">
+            <div className="mt-6 flex items-center justify-between">
               <h1 className="text-2xl font-bold">Henrik Engqvist</h1>
             </div>
             <h3 className="mt-2 text-lg">Frontend Developer</h3>
             <p className="my-6 max-w-2xl text-foreground/80">
-              Hey 👋 I'm Henrik, a Frontend Developer with a focus around UX & accessibility, with previous experience around <span className="text-foreground font-semibold">software development</span> & <span className="text-foreground font-semibold">game development</span>.  I'm currently studying at <span className="text-foreground font-semibold">IT-Högskolan</span> where I'm sharpening my web dev skills.
+              Hey 👋 I'm Henrik, a Frontend Developer with a focus around UX &
+              accessibility, with previous experience around{" "}
+              <span className="font-semibold text-foreground">
+                software development
+              </span>{" "}
+              &{" "}
+              <span className="font-semibold text-foreground">
+                game development
+              </span>
+              . I'm currently studying at{" "}
+              <span className="font-semibold text-foreground">
+                IT-Högskolan
+              </span>{" "}
+              where I'm sharpening my web dev skills.
             </p>
             <div className="flex flex-wrap items-center gap-2">
               <LinkButton link="#" text="Resume" />
               <IconButton icon="github" link="https://github.com/hydrarguru" />
-              <IconButton icon="linkedin" link="https://www.linkedin.com/in/henrik-engqvist/" />
+              <IconButton
+                icon="linkedin"
+                link="https://www.linkedin.com/in/henrik-engqvist/"
+              />
               <IconButton icon="email" link="mailto:henrik@engqvist.org" />
             </div>
-            <div className="hidden md:flex flex-col text-md space-y-2 rounded max-w-2xl text-foreground/70 my-7">
-              <p><span className="font-semibold text-primary/90">Frontend: </span>
-                React.js, Next.js, TailwindCSS, Vue.js.
+            <div className="text-md my-7 hidden max-w-2xl flex-col space-y-2 rounded text-foreground/70 md:flex">
+              <p>
+                <span className="font-semibold text-primary/90">
+                  Frontend:{" "}
+                </span>
+                React.js, Next.js, Vue.js, TailwindCSS.
               </p>
 
-              <p><span className="font-semibold text-primary/90">Backend: </span>
+              <p>
+                <span className="font-semibold text-primary/90">Backend: </span>
                 Express.js, MongoDB, Firebase.
               </p>
 
-              <p><span className="font-semibold text-primary/90">Languages: </span>
+              <p>
+                <span className="font-semibold text-primary/90">
+                  Languages:{" "}
+                </span>
                 JavaScript/TypeScript, C#, C++, SQL, HTML & CSS.
               </p>
 
-              <p><span className="font-semibold text-primary/90">Game Engines: </span>
+              <p>
+                <span className="font-semibold text-primary/90">
+                  Game Engines:{" "}
+                </span>
                 Unity Engine, Unreal Engine.
               </p>
 
-              <p><span className="font-semibold text-primary/90">Tools: </span>
-                Git, JIRA,  Figma, Node.js, Bun, Docker.
+              <p>
+                <span className="font-semibold text-primary/90">Tools: </span>
+                Git, JIRA, Figma, Node.js, Bun, Docker.
               </p>
 
-              <p><span className="font-semibold text-primary/90">Agile methods: </span>
+              <p>
+                <span className="font-semibold text-primary/90">
+                  Agile methods:{" "}
+                </span>
                 Scrum, Kanban.
               </p>
             </div>
           </section>
+        </div>
+        <div className="relative mx-auto mt-3 w-full max-w-4xl p-2 md:p-8 lg:mt-0 lg:h-full lg:w-3/5 lg:overflow-y-scroll">
+          <NavbarMenu projects={<div>Projects</div>} experience={<div>Experience</div>} education={<div>Education</div>
+          } />
         </div>
       </div>
     </main>
---
+++ src/components/NavigationMenu.tsx
@@ -0,0 +1,32 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+type NavbarMenuProps = {
+    projects: React.ReactNode;
+    experience: React.ReactNode;
+    education: React.ReactNode;
+};
+
+function NavbarMenu({ projects, experience, education }: NavbarMenuProps) {
+  return (
+    <div className="sticky top-0 z-50 inline-flex items-center rounded-lg text-md">
+      <Tabs defaultValue="projects" className="w-[800px]">
+        <TabsList className="grid w-full grid-cols-3">
+          <TabsTrigger value="projects">Projects</TabsTrigger>
+          <TabsTrigger value="experience">Experience</TabsTrigger>
+          <TabsTrigger value="education">Education</TabsTrigger>
+        </TabsList>
+        <TabsContent value="projects">
+            {projects}
+        </TabsContent>
+        <TabsContent value="experience">
+            {experience}
+        </TabsContent>
+        <TabsContent value="education">
+            {education}
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
+}
+
+export default NavbarMenu;
