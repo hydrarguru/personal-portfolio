e03ec87175af96040f3cb6c42baa4ef94aa71504
hydrarguru <henrik@engqvist.org>
2024-08-25 21:34:03 +0200

    feat: IconButton component.

---
+++ src/components/IconButton.tsx
@@ -0,0 +1,39 @@
+import { GitHubLogoIcon, EnvelopeClosedIcon, LinkedInLogoIcon, ExclamationTriangleIcon } from "@radix-ui/react-icons";
+import { Button } from "@/components/ui/button";
+
+type IconButtonProps = {
+    icon: string;
+    link: string;
+};
+
+function IconButton({ icon, link }: IconButtonProps) {
+
+    switch (icon) {
+        case "github":
+            return (
+                <Button variant="outline" size="icon" onClick={() => window.open(link, "_blank")}>
+                    <GitHubLogoIcon className="h-4 w-4" />
+                </Button>
+            );
+        case "email":
+            return (
+                <Button variant="outline" size="icon" onClick={() => window.open(link, "_self")}>
+                    <EnvelopeClosedIcon className="h-4 w-4" />
+                </Button>
+            );
+        case "linkedin":
+            return (
+                <Button variant="outline" size="icon" onClick={() => window.open(link, "_blank")}>
+                    <LinkedInLogoIcon className="h-4 w-4" />
+                </Button>
+            );
+        default:
+            return (
+                <Button variant="outline" size="icon">
+                    <ExclamationTriangleIcon className="h-4 w-4" />
+                </Button>
+            );
+    }
+}
+
+export default IconButton;
