832f418ac0c4facdb91414e9da87a56a257b954e
hydrarguru <henrik@engqvist.org>
2024-08-28 23:27:15 +0200

    feat: Add Project type definition

---
+++ src/types/types.ts
@@ -0,0 +1,8 @@
+export type Project = {
+    projectTitle: string;
+    projectDescription: string;
+    projectTags: string[];
+    projectDate: string;
+    githubLink: string;
+    websiteLink: string;
+}
