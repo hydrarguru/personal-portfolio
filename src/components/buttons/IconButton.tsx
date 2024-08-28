71543590304c247b5103ccec5427f4fbf3e503e8
hydrarguru <henrik@engqvist.org>
2024-08-28 23:14:29 +0200

    feat: Add globe icon to IconButton component

--- src/components/buttons/IconButton.tsx
+++ src/components/buttons/IconButton.tsx
@@ -1,4 +1,4 @@
-import { GitHubLogoIcon, EnvelopeClosedIcon, LinkedInLogoIcon, ExclamationTriangleIcon } from "@radix-ui/react-icons";
+import { GitHubLogoIcon, EnvelopeClosedIcon, LinkedInLogoIcon, GlobeIcon, ExclamationTriangleIcon } from "@radix-ui/react-icons";
 import { Button } from "@/components/ui/button";
 
 type IconButtonProps = {
@@ -27,6 +27,14 @@ function IconButton({ icon, link }: IconButtonProps) {
                     <LinkedInLogoIcon className="h-4 w-4" />
                 </Button>
             );
+        case "globe":
+        return (
+            <Button variant="outline" size="icon" onClick={() => window.open(link, "_blank")}>
+                <GlobeIcon className="h-4 w-4" />
+            </Button>
+        );
+
+        
         default:
             return (
                 <Button variant="outline" size="icon">
