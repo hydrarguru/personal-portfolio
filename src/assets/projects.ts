34d12c6541702fca78fc072e9984d195c708c1cc
hydrarguru <henrik@engqvist.org>
2024-08-28 23:27:44 +0200

    feat: Add projects.ts file with project data.

---
+++ src/assets/projects.ts
@@ -0,0 +1,28 @@
+import { Project } from "@/types/types";
+
+export const projects: Project[] = [
+    {
+        projectTitle: "Project Title",
+        projectDescription: "Project Description",
+        projectTags: ["React", "TypeScript", "TailwindCSS"],
+        projectDate: "20 August 2024",
+        githubLink: "",
+        websiteLink: ""
+    },
+    {
+        projectTitle: "Project Title",
+        projectDescription: "Project Description",
+        projectTags: ["React", "TypeScript", "TailwindCSS"],
+        projectDate: "20 August 2024",
+        githubLink: "",
+        websiteLink: ""
+    },
+    {
+        projectTitle: "Project Title",
+        projectDescription: "Project Description",
+        projectTags: ["React", "TypeScript", "TailwindCSS"],
+        projectDate: "20 August 2024",
+        githubLink: "",
+        websiteLink: ""
+    }
+]
