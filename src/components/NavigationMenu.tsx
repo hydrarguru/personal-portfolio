cb47ce9fcf3097a8f4e3ebbac700b227325fc309
hydrarguru <henrik@engqvist.org>
2024-08-30 15:55:40 +0200

    Updated NavigationMenu component to improve layout and responsiveness

--- src/components/NavigationMenu.tsx
+++ src/components/NavigationMenu.tsx
@@ -8,25 +8,28 @@ type NavbarMenuProps = {
 
 function NavbarMenu({ projects, experience, education }: NavbarMenuProps) {
   return (
-    <div className="sticky top-0 z-50 inline-flex items-center rounded-lg text-md">
-      <Tabs defaultValue="projects" className="w-[600px]">
-        <TabsList className="grid w-full grid-cols-3">
-          <TabsTrigger value="projects">Projects</TabsTrigger>
-          <TabsTrigger value="experience">Experience</TabsTrigger>
-          <TabsTrigger value="education">Education</TabsTrigger>
-        </TabsList>
-        <TabsContent value="projects">
+    <div className="sticky top-0 z-50">
+      <div className="container pr-4 pl-4">
+        <Tabs defaultValue="projects">
+          <TabsList className="grid w-full grid-cols-3">
+            <TabsTrigger value="projects">Projects</TabsTrigger>
+            <TabsTrigger value="experience">Experience</TabsTrigger>
+            <TabsTrigger value="education">Education</TabsTrigger>
+          </TabsList>
+          <TabsContent value="projects">
             {projects}
-        </TabsContent>
-        <TabsContent value="experience">
+          </TabsContent>
+          <TabsContent value="experience">
             {experience}
-        </TabsContent>
-        <TabsContent value="education">
+          </TabsContent>
+          <TabsContent value="education">
             {education}
-        </TabsContent>
-      </Tabs>
+          </TabsContent>
+        </Tabs>
+      </div>
     </div>
   );
 }
-
+//container mx-auto
+//lg:w-[780px] md:
 export default NavbarMenu;
