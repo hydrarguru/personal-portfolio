cb81c8bc47953f6f7d0d0d2b3030486168f1b743
hydrarguru <henrik@engqvist.org>
2024-10-03 14:54:50 +0200

    added search bar component to ProjectTab to allow users search for projects.

--- src/components/tabs/ProjectTab.tsx
+++ src/components/tabs/ProjectTab.tsx
@@ -1,20 +1,56 @@
-import ProjectCard from "../cards/ProjectCard";
+import { useEffect, useState } from "react";
+
+import { Project } from "../../types/types";
 import { projects } from "@/assets/projects";
 
+import ProjectCard from "../cards/ProjectCard";
+import SearchBar from "../nav/SearchBar";
+
+
+
 function ProjectTab() {
+  const [projectData, setProjectData] = useState<Project[]>([]);
+  const [searchResults, setSearchResults] = useState<Project[]>([]);
+
+  useEffect(() => {
+    setProjectData(projects);
+    setSearchResults(projects);
+  }, []);
+
+  function filterProjects(searchTerm: string) {
+    const results = projectData.filter((project) => {
+      return project.projectTitle.toLowerCase().includes(searchTerm.toLowerCase());
+    });
+    setSearchResults(results);
+  }
+
+  function handleSearch(searchTerm: string) {
+    console.log('Search Term: ', searchTerm);
+    if (searchTerm.length === 0) {
+      setSearchResults(projectData);
+      return;
+    }
+    else {
+      filterProjects(searchTerm);
+    }
+  }
+  
   return (
     <section className="mt-5 w-full space-y-6">
-      {projects.map((project, index) => (
-        <ProjectCard
-          key={index}
-          projectTitle={project.projectTitle}
-          projectDescription={project.projectDescription}
-          projectTags={project.projectTags}
-          githubLink={project.githubLink || ""}
-          websiteLink={project.websiteLink || ""}
-          youtubeLink={project.youtubeLink || ""}
-        />
-      ))}
+      <SearchBar onSearch={handleSearch} />
+      {
+        searchResults.map((project) => (
+          <ProjectCard
+            key={project.projectTitle}
+            projectTitle={project.projectTitle}
+            projectDescription={project.projectDescription}
+            projectTags={project.projectTags}
+            githubLink={project.githubLink || ""}
+            websiteLink={project.websiteLink || ""}
+            youtubeLink={project.youtubeLink || ""}
+          />
+        ))
+      }
     </section>
   );
 }
