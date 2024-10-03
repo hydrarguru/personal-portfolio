7eb80646416f0bb53c348c643fbb976ed44baa04
hydrarguru <henrik@engqvist.org>
2024-10-03 14:54:07 +0200

    SearchBar component

---
+++ src/components/nav/SearchBar.tsx
@@ -0,0 +1,40 @@
+import { useState, useCallback } from "react";
+import { debounce } from "lodash";
+import { Input } from "@/components/ui/input";
+import IconButton from "../buttons/IconButton";
+
+interface SearchBarProps {
+  onSearch: (searchTerm: string) => void;
+}
+
+function SearchBar({ onSearch }: SearchBarProps) {
+  const [searchQuery, setSearchQuery] = useState<string>("");
+
+  const debouncedSearch = useCallback(
+    debounce((searchTerm: string) => {
+      onSearch(searchTerm);
+    }, 500),
+    [searchQuery],
+  );
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setSearchQuery(value);
+    debouncedSearch(value);
+  };
+
+  return (
+    <div className="flex items-center space-x-2">
+      <Input
+        className="focus-visible:ring-0 flex-grow"
+        type="text"
+        placeholder="Search for projects"
+        onChange={handleChange}
+        autoComplete="false"
+      />
+      <IconButton icon="filter" />
+    </div>
+  );
+}
+
+export default SearchBar;
