e612fe3d77d1e7e8f7585601fca9f262d3500004
hydrarguru <henrik@engqvist.org>
2024-10-03 13:04:43 +0200

    Added new LinkToolTip component for previewing links on hover.

---
+++ src/components/buttons/LinkToolTip.tsx
@@ -0,0 +1,26 @@
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+type LinkToolTipProps = {
+  link: string;
+  children: JSX.Element;
+};
+
+function LinkToolTip({ link, children }: LinkToolTipProps) {
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger>{children}</TooltipTrigger>
+        <TooltipContent>
+          <p>{link}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+}
+
+export default LinkToolTip;
