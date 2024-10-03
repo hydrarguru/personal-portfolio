aaa5fe6053797df42e01f2065dc28c822e1f73ed
hydrarguru <henrik@engqvist.org>
2024-10-03 12:24:35 +0200

    Added game projects to list of projects.

--- src/assets/projects.ts
+++ src/assets/projects.ts
@@ -35,5 +35,29 @@ export const projects: Project[] = [
         projectTags: ["TypeScript", "Chart.js", "IndexedDB API"],
         githubLink: "https://github.com/hydrarguru/job-application-tracker",
         websiteLink: ""
+    },
+    {
+        projectTitle: "Fennec Peak",
+        projectDescription: "Fennec Peak is a local multiplayer battle game that features four different adventures, some looking for wealth, some looking for fame, some looking for knowledge; They must now fight eachother to claim their glory.",
+        projectTags: ["Unity Engine", "C#"],
+        githubLink: "",
+        websiteLink: "",
+        youtubeLink: "https://www.youtube.com/watch?v=ldgcvd1pesg"
+    },
+    {
+        projectTitle: "Dayspring",
+        projectDescription: "Stealth/puzzle platformer in a nordic setting. Hide from monsters in the dark as you make your way through each level following the journey of a young child reliving the tales of their grandfather's old bedtime stories",
+        projectTags: ["Unity Engine", "C#"],
+        githubLink: "",
+        websiteLink: "",
+        youtubeLink: "https://www.youtube.com/watch?v=4F8_xEsvPfQ"
+    },
+    {
+        projectTitle: "Summit",
+        projectDescription: "Summit is a single player adventure game with a slight hint of action. In Summit you play as Riq after a crashlanding on top of the tower. Your objective is to get to the top and fly back home. And on the way throught the treacherous tower will Riq meet either his doom or salvation.",
+        projectTags: ["Unreal Engine", "C++"],
+        githubLink: "",
+        websiteLink: "",
+        youtubeLink: "https://www.youtube.com/watch?v=d8DZ9ZOvlG8"
     }
 ]
