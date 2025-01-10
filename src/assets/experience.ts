a1f6402eefb3f22523ac715667cf89c9ad12b73c
hydrarguru <henrik@engqvist.org>
2025-01-10 01:36:06 +0100

    feat: Experience related components.
    - Experience type.
    - ExperienceCard component.
    - Experience data.

---
+++ src/assets/experience.ts
@@ -0,0 +1,27 @@
+import { Experience } from "@/types/types";
+
+export const experiences: Experience[] = [
+    {
+        experienceTitle: "Traino",
+        experienceRole: "Frontend Developer",
+        experienceDescription: `
+            Reported and fixed various types of bugs.
+            Collaborated with other developers on a larger code base and developed new functionality and extended existing functionality.
+            Restructured and rebuilt a larger dynamic calendar component with schedule creation & booking capabilities using React.
+            Implemented backend functionality around the dynamic calendar component using PHP.Reported 
+            `,
+        startDate: "Aug 2024",
+        endDate: "Nov 2024"
+    },
+    {
+        experienceTitle: "House of How",
+        experienceRole: "Tools Developer",
+        experienceDescription: `
+            Developing & maintaining new tools for artists & other disciplines using C# & WPF.
+            Extending, adding new features & making QoL Improvements to existing tools.
+            Modified an existing tool built in Unity & C# for generating random dungeon maps to include support for random generated parkour maps.
+            `,
+        startDate: "Jun 2020",
+        endDate: "Nov 2020"
+    }
+];
---
+++ src/components/cards/ExperienceCard.tsx
@@ -0,0 +1,40 @@
+import { Separator } from '../ui/separator';
+
+interface ExperienceCardProps {
+  experienceTitle: string;
+  experienceRole: string;
+  experienceDescription: string;
+  startDate: string;
+  endDate: string;
+  image?: string;
+}
+
+function ExperienceCard({
+  experienceTitle,
+  experienceRole,
+  experienceDescription,
+  startDate,
+  endDate,
+}: ExperienceCardProps) {
+  return (
+    <div className="flex justify-between gap-2 p-4 overflow-hidden rounded-xl border hover:border-green-700 transition duration-300 ease-in-out">
+      <div className="tablet:w-3/5 w-full space-y-2">
+        <a className="group/link space-y-2">
+          <div className="flex items-center justify-between">
+            <h1 className="font-heading ml-2 text-xl font-semibold">
+              {experienceTitle}
+            </h1>
+            <span className="text-xl ml-2 font-semibold">{experienceRole}</span>
+            <span className="">{`${startDate} - ${endDate}`}</span>
+          </div>
+          <Separator />
+          <p className="text-md max-w-4xl text-muted-foreground">
+            {experienceDescription}
+          </p>
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default ExperienceCard;
