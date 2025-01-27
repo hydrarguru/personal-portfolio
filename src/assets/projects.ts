c96a5b6423890dbf0113dce07da6b9b539e9fb1c
hydrarguru <henrik@engqvist.org>
2025-01-27 15:49:08 +0100

    chore: updated projects with more tags.

--- src/assets/projects.ts
+++ src/assets/projects.ts
@@ -4,7 +4,7 @@ export const projects: Project[] = [
     {
         projectTitle: "Reader",
         projectDescription: "Reader is a web app that allows users to join communities & share their interests with others.",
-        projectTags: ["React", "TypeScript"],
+        projectTags: ["React", "TypeScript", "HTML", "CSS", "TailwindCSS", "shadcn"],
         githubLink: "https://github.com/hydrarguru/reader",
         websiteLink: "https://reader-app.netlify.app/"
     },
@@ -18,21 +18,21 @@ export const projects: Project[] = [
     {
         projectTitle: "CoinCompass",
         projectDescription: "CoinCompass is a web application with the goal of helping you save money by tracking your expenses.",
-        projectTags: ["Vue", "TypeScript", "Firebase"],
+        projectTags: ["Vue", "TypeScript", "HTML", "CSS",  "Firebase"],
         githubLink: "https://github.com/ITHS-Team8/CoinCompass",
         websiteLink: "https://coincompass.netlify.app/"
     },
     {
         projectTitle: "OGLe (Online Game Library)",
         projectDescription: "An online video game library that consumes the RAWG API to display information about games.",
-        projectTags: ["Vue", "TypeScript"],
+        projectTags: ["Vue", "TypeScript", "HTML", "CSS"],
         githubLink: "https://github.com/hydrarguru/ogle",
         websiteLink: "https://hg-oogle.netlify.app/"
     },
     {
         projectTitle: "J.A.T. (Job Application Tracker)",
         projectDescription: "Web extension that helps you keep track of your job applications.",
-        projectTags: ["TypeScript", "Chart.js", "IndexedDB API"],
+        projectTags: ["TypeScript", "HTML", "CSS", "Chart.js", "IndexedDB API"],
         githubLink: "https://github.com/hydrarguru/job-application-tracker",
         websiteLink: ""
     },
